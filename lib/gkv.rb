89e641df518b4c82dbac824f549a57e6df1f047b
= <=>
2015-06-16 17:44:38 -0400

    minimal v1

---
+++ lib/gkv.rb
@@ -0,0 +1,41 @@
+#require "gkv/version"
+
+module Gkv
+  class Database
+    attr_accessor :items
+
+    def initialize
+      `git init`
+      @items = {}
+    end
+
+    def set(key, value)
+      @items[key] = hash_object(value)
+    end
+
+    def get(key)
+      cat_file(@items[key.to_s])
+    end
+
+    def hash_object(data)
+      write_tmpfile(data)
+      hash = `git hash-object -w tmp.txt`.strip!
+      File.delete('tmp.txt') 
+      hash
+    end
+    
+    def write_tmpfile(data)
+      f = File.open("tmp.txt", "w+")
+      f.write(data.to_s)
+      f.close
+    end
+
+    def cat_file(hash)
+      `git cat-file -p #{hash}`
+    end
+
+    def versions(key)
+      @items[key].length
+    end
+  end
+end
---
+++ lib/gkv/version.rb
@@ -0,0 +1,3 @@
+module Gkv
+  VERSION = "0.1.0"
+end
