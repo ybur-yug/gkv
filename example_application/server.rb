ef18595090c3ddbc19d13d088ba2fa9343c810a5
= <=>
2015-06-17 19:05:03 -0400

    fix errors

--- example_application/server.rb
+++ example_application/server.rb
@@ -5,12 +5,13 @@ require 'sinatra'
 db = Gkv::Database.new
 
 get '/get/:key' do
-  db.get(params['key']
+  db.get(params['key'])
 end
 
-post '/set'
-  key, value = params.fetch('key'), params.fetch('value')
-  db.set(key, value)
+post '/set' do
+  begin
+    key, value = params.fetch('key'), params.fetch('value')
+    db.set(key, value)
   rescue KeyError
     { error: 'Please send key and value params in your request' }.to_json
   end
