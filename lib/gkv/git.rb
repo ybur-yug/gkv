e5546e275a1e9eb21b583fb7a71b01fc78a57d20
= <=>
2015-06-20 19:34:44 -0400

    lol oops

--- lib/gkv/git.rb
+++ lib/gkv/git.rb
@@ -2,14 +2,14 @@ module Gkv
   module GitFunctions
     extend self
 
-    def self.hash_object(data)
+    def hash_object(data)
       write_tmpfile(data)
       hash = `git hash-object -w tmp.txt`.strip!
       File.delete('tmp.txt')
       hash
     end
 
-    def self.write_tmpfile(data)
+    def write_tmpfile(data)
       f = File.open('tmp.txt', 'w+')
       f.write(data.to_s)
       f.close
